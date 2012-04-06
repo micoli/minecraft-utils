3d4a06252700323e48d158b69581dd4b248b8d80
micoli <olivier@micoli.org>
2012-04-06 02:35:10 +0200

    commands with annonation

---
+++ src/main/java/org/micoli/minecraft/bukkit/QDCommand.java
@@ -0,0 +1,17 @@
+package org.micoli.minecraft.bukkit;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface QDCommand {
+	public enum SenderType { PLAYER, CONSOLE, BOTH };
+
+	public String aliases();
+	public String description() default  "";
+	public String help() default "";
+	public SenderType senderType() default SenderType.PLAYER;
+}
