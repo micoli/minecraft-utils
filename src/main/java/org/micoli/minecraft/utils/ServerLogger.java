7ffaef79458b266ace8b18d5a27ba0d93d3997ae
micoli <olivier@micoli.org>
2012-05-05 02:30:52 +0200

    dumpStackTrace

--- src/main/java/org/micoli/minecraft/utils/ServerLogger.java
+++ src/main/java/org/micoli/minecraft/utils/ServerLogger.java
@@ -1,5 +1,8 @@
 package org.micoli.minecraft.utils;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.io.Writer;
 import java.util.logging.Logger;
 
 import com.google.gson.Gson;
@@ -10,54 +13,67 @@ import com.google.gson.GsonBuilder;
  * The Class ServerLogger.
  */
 public class ServerLogger {
-	
+
 	/** The logger. */
 	private Logger logger = Logger.getLogger("Minecraft");
-	
+
 	/** The prefix. */
 	private String prefix = "plugin";
 
 	/**
 	 * Sets the prefix.
-	 *
-	 * @param prf the new prefix
+	 * 
+	 * @param prf
+	 *            the new prefix
 	 */
-	public void setPrefix(String prf){
+	public void setPrefix(String prf) {
 		prefix = prf;
 	}
-	
+
 	/**
 	 * Log.
-	 *
-	 * @param str the str
+	 * 
+	 * @param str
+	 *            the str
 	 */
 	public void log(String str) {
-		logger.info("["+prefix+"] "+str);
+		logger.info("[" + prefix + "] " + str);
 	}
 
 	public void log(String[] str) {
-		for(String s:str){
-			logger.info("["+prefix+"] "+s);
+		for (String s : str) {
+			logger.info("[" + prefix + "] " + s);
 		}
 	}
+
 	/**
 	 * Log.
-	 *
-	 * @param str the str
-	 * @param args the args
+	 * 
+	 * @param str
+	 *            the str
+	 * @param args
+	 *            the args
 	 */
-	public void log(String str,Object... args) {
-		logger.info("["+prefix+"] "+ChatFormater.formatClean(str, args));
+	public void log(String str, Object... args) {
+		logger.info("[" + prefix + "] " + ChatFormater.formatClean(str, args));
 	}
-	
+
 	/**
 	 * Dump.
-	 *
-	 * @param obj the obj
+	 * 
+	 * @param obj
+	 *            the obj
 	 */
-	public void dump(Object obj){
+	public void dump(Object obj) {
 		Gson gson = new GsonBuilder().setPrettyPrinting().create();
-		logger.info("["+prefix+"] "+obj.getClass()+"\n"+gson.toJson(obj));
+		logger.info("[" + prefix + "] " + obj.getClass() + "\n" + gson.toJson(obj));
+	}
+
+	public void dumpStackTrace(Exception e) {
+		final Writer result = new StringWriter();
+		final PrintWriter printWriter = new PrintWriter(result);
+		e.printStackTrace(printWriter);
+		logger.severe(result.toString());
 	}
 
 }
